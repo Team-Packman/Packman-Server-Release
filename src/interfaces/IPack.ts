1fe4c8fe5e8ce7106e2ff5b8a8bce75a5c13be8e
dingding-21 <phj8221@naver.com>
2022-08-14 07:59:12 +0900

    [FEAT]/#9 함께패킹리스트 짐 수정 interface

--- src/interfaces/IPack.ts
+++ src/interfaces/IPack.ts
@@ -4,6 +4,14 @@ export interface PackCreateDto {
   listId: string;
 }
 
+export interface PackUpdateDto {
+  id: string;
+  name: string;
+  isChecked: boolean;
+  listId: string;
+  categoryId: string;
+}
+
 export interface PackResponseDto {
   id: string;
   category: [
