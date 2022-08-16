aeacf10f027016c5d0af024670a8bd1d1227f027
dingding-21 <phj8221@naver.com>
2022-08-16 13:33:18 +0900

    [FEAT]/#15 최근 생성 리스트 조회 204처리 관련 메시지 추가

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -32,6 +32,7 @@ const message = {
   SUCCESS_GET_RECENT_CREATED_LIST: '최근 생성된 리스트 조회 성공',
 
   //패킹리스트 공통
+  NO_USER_LIST: '유저가 생성한 리스트가 없습니다',
   NO_PACKINGLIST: '존재하지 않는 PackingList입니다',
   DUPLICATION_PACKINGLIST: '중복된 리스트 이름입니다',
   NO_UPDATEDTITLE: '존재하지 않는 UpdatedTitle입니다',
