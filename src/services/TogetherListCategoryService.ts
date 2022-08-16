50749bf9a7c0f459d87293c03cae7903065cb91f
kkl4846 <kkl4846@cau.ac.kr>
2022-08-16 15:34:01 +0900

    [FEAT]/#19 order by id 추가

--- src/services/TogetherListCategoryService.ts
+++ src/services/TogetherListCategoryService.ts
@@ -52,13 +52,14 @@ const createCategory = async (
         'isChecked', p.is_checked, 
         'packer', 
         CASE WHEN (u.id IS NOT NULL) THEN json_build_object('id', u.id::text, 'nickname', u.nickname)
-        END
-        ))FILTER (WHERE p.id IS NOT NULL), '[]') AS pack
+        END) ORDER BY p.id
+      ) FILTER (WHERE p.id IS NOT NULL), '[]') AS pack
     FROM "category" as c 
     LEFT JOIN "pack" as p ON c.id = p.category_id
     LEFT JOIN "user" as u ON u.id = p.packer_id
     WHERE c.list_id = $1
     GROUP BY c.id
+    ORDER BY c.id
     `,
       [categoryCreateDto.listId],
     );
@@ -138,20 +139,21 @@ const updateCategory = async (
 
     const { rows: categorys } = await client.query(
       `
-      SELECT c.id::text, c.name,  COALESCE(JSON_AGG(json_build_object(
-          'id', p.id::text,
-          'name', p.name, 
-          'isChecked', p.is_checked, 
-          'packer', 
-          CASE WHEN (u.id IS NOT NULL) THEN json_build_object('id', u.id::text, 'nickname', u.nickname)
-          END
-          ))FILTER (WHERE p.id IS NOT NULL), '[]') AS pack
-      FROM "category" as c 
-      LEFT JOIN "pack" as p ON c.id = p.category_id
-      LEFT JOIN "user" as u ON u.id = p.packer_id
-      WHERE c.list_id = $1
-      GROUP BY c.id
-      `,
+    SELECT c.id::text, c.name,  COALESCE(JSON_AGG(json_build_object(
+        'id', p.id::text,
+        'name', p.name, 
+        'isChecked', p.is_checked, 
+        'packer', 
+        CASE WHEN (u.id IS NOT NULL) THEN json_build_object('id', u.id::text, 'nickname', u.nickname)
+        END) ORDER BY p.id
+      ) FILTER (WHERE p.id IS NOT NULL), '[]') AS pack
+    FROM "category" as c 
+    LEFT JOIN "pack" as p ON c.id = p.category_id
+    LEFT JOIN "user" as u ON u.id = p.packer_id
+    WHERE c.list_id = $1
+    GROUP BY c.id
+    ORDER BY c.id
+    `,
       [categoryUpdateDto.listId],
     );
 
@@ -209,20 +211,21 @@ const deleteCategory = async (
 
     const { rows: categorys } = await client.query(
       `
-      SELECT c.id::text, c.name,  COALESCE(JSON_AGG(json_build_object(
-          'id', p.id::text,
-          'name', p.name, 
-          'isChecked', p.is_checked, 
-          'packer', 
-          CASE WHEN (u.id IS NOT NULL) THEN json_build_object('id', u.id::text, 'nickname', u.nickname)
-          END
-          ))FILTER (WHERE p.id IS NOT NULL), '[]') AS pack
-      FROM "category" as c 
-      LEFT JOIN "pack" as p ON c.id = p.category_id
-      LEFT JOIN "user" as u ON u.id = p.packer_id
-      WHERE c.list_id = $1
-      GROUP BY c.id
-      `,
+    SELECT c.id::text, c.name,  COALESCE(JSON_AGG(json_build_object(
+        'id', p.id::text,
+        'name', p.name, 
+        'isChecked', p.is_checked, 
+        'packer', 
+        CASE WHEN (u.id IS NOT NULL) THEN json_build_object('id', u.id::text, 'nickname', u.nickname)
+        END) ORDER BY p.id
+      ) FILTER (WHERE p.id IS NOT NULL), '[]') AS pack
+    FROM "category" as c 
+    LEFT JOIN "pack" as p ON c.id = p.category_id
+    LEFT JOIN "user" as u ON u.id = p.packer_id
+    WHERE c.list_id = $1
+    GROUP BY c.id
+    ORDER BY c.id
+    `,
       [categoryDeleteDto.listId],
     );
 
