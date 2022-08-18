7e1bee805f4b8e5bfa847d7755073438da8d20d8
dingding-21 <phj8221@naver.com>
2022-08-18 17:35:27 +0900

    [FEAT]/#27 getNewToken Controller 구현

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,8 @@ import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
 import { AuthService } from '../services';
+import jwtHandler from '../modules/jwtHandler';
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const db = require('../loaders/db');
 /**
@@ -39,6 +41,62 @@ const getKakaoUser = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @route GET /auth/token
+ *  @desc get new token
+ *  @access private
+ **/
+
+const getNewToken = async (req: Request, res: Response) => {
+  let client;
+  const accessToken = req.headers['authorization']?.split(' ').reverse()[0] as string;
+  const refreshToken = req.headers.refresh as string;
+
+  if (!accessToken || !refreshToken) {
+    res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE_TOKEN));
+  }
+  try {
+    client = await db.connect(req);
+
+    const data = await AuthService.getNewToken(client, accessToken, refreshToken);
+
+    if (data === 'no_user')
+      return res
+        .status(statusCode.UNAUTHORIZED)
+        .send(util.fail(statusCode.UNAUTHORIZED, message.NO_USER));
+    else if (data === 'no_user_refresh')
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_USER_RF_TOKEN));
+    else if (data === 'invalid_token')
+      return res
+        .status(statusCode.UNAUTHORIZED)
+        .send(util.fail(statusCode.UNAUTHORIZED, message.INVALID_TOKEN));
+    else if (data === 'all_expired_token')
+      return res
+        .status(statusCode.UNAUTHORIZED)
+        .send(util.fail(statusCode.UNAUTHORIZED, message.ALL_EXPIRED_TOKEN));
+    else if (data === 'valid_token')
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.VALID_TOKEN));
+    else
+      res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.SUCCESS_REISSUE_TOKEN, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
+
 export default {
   getKakaoUser,
+  getNewToken,
 };
