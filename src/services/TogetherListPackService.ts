6b766347ee0494b99b6b92953beece1ef3f2fbaf
dingding-21 <phj8221@naver.com>
2022-08-18 22:16:10 +0900

    [STYLE]/#10 쿼리문 백틱 정렬

--- src/services/TogetherListPackService.ts
+++ src/services/TogetherListPackService.ts
@@ -6,49 +6,54 @@ const deletePack = async (
 ): Promise<PackResponseDto | string> => {
   try {
     const { rows: existList } = await client.query(
-      `SELECT *
-     FROM "packing_list" as pl
-     WHERE pl.id = $1
-    `,
+      `
+      SELECT *
+      FROM "packing_list" as pl
+      WHERE pl.id = $1
+      `,
       [packDeleteDto.listId],
     );
 
     if (existList.length === 0) return 'no_list';
 
     const { rows: existCategory } = await client.query(
-      `SELECT *
-     FROM "category" as c
-     WHERE c.id = $1
-    `,
+      `
+      SELECT *
+      FROM "category" as c
+      WHERE c.id = $1
+      `,
       [packDeleteDto.categoryId],
     );
 
     if (existCategory.length === 0) return 'no_category';
 
     const { rows: existPack } = await client.query(
-      `SELECT *
-     FROM "pack" as p
-     WHERE p.id = $1
-    `,
+      `
+      SELECT *
+      FROM "pack" as p
+      WHERE p.id = $1
+      `,
       [packDeleteDto.packId],
     );
 
     if (existPack.length === 0) return 'no_pack';
 
     const { rows: existListCategory } = await client.query(
-      `SELECT *
-     FROM "category" as c
-     WHERE c.id = $1 AND c.list_id = $2
-    `,
+      `
+      SELECT *
+      FROM "category" as c
+      WHERE c.id = $1 AND c.list_id = $2
+      `,
       [packDeleteDto.categoryId, packDeleteDto.listId],
     );
 
     if (existListCategory.length === 0) return 'no_list_category';
     const { rows: existCategoryPack } = await client.query(
-      `SELECT *
-     FROM "pack" as p
-     WHERE p.id = $1 AND p.category_id = $2
-    `,
+      `
+      SELECT *
+      FROM "pack" as p
+      WHERE p.id = $1 AND p.category_id = $2
+      `,
       [packDeleteDto.packId, packDeleteDto.categoryId],
     );
 
@@ -56,30 +61,30 @@ const deletePack = async (
 
     const { rows } = await client.query(
       `
-    DELETE FROM "pack"
-    WHERE id = $1
-    `,
+      DELETE FROM "pack"
+      WHERE id = $1
+      `,
       [packDeleteDto.packId],
     );
 
     const { rows: category } = await client.query(
       `
-    SELECT    c.id,
-    c.name,
-    coalesce(json_agg( json_build_object( 'id', p.ID::text, 'name', p.name, 'is_checked', p.is_checked,'packer',
-    CASE
-              WHEN u.id IS NULL THEN NULL
-              ELSE json_build_object('id', u.ID::text, 'name', u.nickname)
-              end) ORDER BY p.id) filter( WHERE p.id IS NOT NULL ), '[]' ) AS pack
-    FROM      category c
-    LEFT JOIN pack p
-    ON        c.id = p.category_id
-    LEFT JOIN "user" u
-    ON        u.id = p.packer_id
-    WHERE     c.list_id = $1
-    GROUP BY  c.id
-    ORDER BY  c.id
-    `,
+      SELECT    c.id,
+      c.name,
+      coalesce(json_agg( json_build_object( 'id', p.ID::text, 'name', p.name, 'is_checked', p.is_checked,'packer',
+      CASE
+                WHEN u.id IS NULL THEN NULL
+                ELSE json_build_object('id', u.ID::text, 'name', u.nickname)
+                end) ORDER BY p.id) filter( WHERE p.id IS NOT NULL ), '[]' ) AS pack
+      FROM      category c
+      LEFT JOIN pack p
+      ON        c.id = p.category_id
+      LEFT JOIN "user" u
+      ON        u.id = p.packer_id
+      WHERE     c.list_id = $1
+      GROUP BY  c.id
+      ORDER BY  c.id
+      `,
       [packDeleteDto.listId],
     );
 
