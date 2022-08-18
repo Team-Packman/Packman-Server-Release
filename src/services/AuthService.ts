e0ec2f18f40c0ec425177dafe72e463256786897
dingding-21 <phj8221@naver.com>
2022-08-18 17:36:09 +0900

    [FEAT]/#27 getNewToken Service 구현

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import { AuthResponseDto } from '../interfaces/IUser';
-import getToken from '../modules/jwtHandler';
+import { AuthResponseDto, AuthTokenResponseDto } from '../interfaces/IAuth';
+import jwtHandler from '../modules/jwtHandler';
 import UserService from './UserService';
+import jwt from 'jsonwebtoken';
 
 const getKakaoUser = async (
   client: any,
@@ -39,7 +40,8 @@ const getKakaoUser = async (
       if (userInfo[0].is_deleted) {
         await UserService.deleteUser(client, userEmail);
       } else {
-        const accessToken = getToken(userInfo[0].id);
+        const accessToken = jwtHandler.getAccessToken(userInfo[0].id);
+        const refreshToken = jwtHandler.getRefreshToken();
         data = {
           isAlreadyUser: true,
           id: userInfo[0].id.toString(),
@@ -48,6 +50,7 @@ const getKakaoUser = async (
           email: userInfo[0].email,
           profileImage: userInfo[0].profile_image,
           accessToken: accessToken,
+          refreshToken: refreshToken,
         };
       }
     }
@@ -56,6 +59,67 @@ const getKakaoUser = async (
     console.log(error);
   }
 };
+
+const getNewToken = async (
+  client: any,
+  accessToken: string,
+  refreshToken: string,
+): Promise<AuthTokenResponseDto | string | undefined> => {
+  try {
+    const decodedAc = jwtHandler.verifyToken(accessToken);
+    const decodedRf = jwtHandler.verifyToken(refreshToken);
+    const decoded = jwt.decode(accessToken);
+
+    if (decodedAc === 'invalid_token' || decodedRf === 'invalid_token') return 'invalid_token';
+
+    const userId = (decoded as any).user.id;
+
+    const { rows: existUser } = await client.query(
+      `
+      SELECT *
+      FROM "user" u
+      WHERE u.id = $1 and is_deleted = false
+      `,
+      [userId],
+    );
+
+    if (existUser.length === 0) return 'no_user';
+
+    const { rows: refresh } = await client.query(
+      `
+      SELECT u.refresh_token
+      FROM "user" u
+      WHERE u.id = $1
+    `,
+      [userId],
+    );
+
+    if (refresh[0].refresh_token !== refreshToken) return 'no_user_refresh';
+
+    if (decodedAc === 'expired_token') {
+      //accessToken이 만료되고 refreshToken도 만료된 경우
+      if (decodedRf === 'expired_token') {
+        return 'all_expired_token';
+      } else {
+        // accessToken이 만료되고 refreshToken은 만료되지 않은 경우
+        const newAcToken = jwtHandler.getAccessToken(userId);
+
+        const data: AuthTokenResponseDto = {
+          accessToken: newAcToken,
+          refreshToken,
+        };
+        return data;
+      }
+    }
+
+    // accessToken이 만료되지 않은 경우
+    return 'valid_token';
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default {
   getKakaoUser,
+  getNewToken,
 };
