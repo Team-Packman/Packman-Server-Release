528c544193915df10eea3c8108e6627df3f2d6c7
laalaa31 <jseoh4298@gmail.com>
2022-08-19 19:54:35 +0900

    [CHORE]/#23 merge 위한 수정

--- src/controllers/TogetherListController.ts
+++ src/controllers/TogetherListController.ts
@@ -9,60 +9,6 @@ import TogetherListService from '../services/TogetherListService';
 const db = require('../loaders/db');
 
 /**
- *  @route POST /list/together
- *  @desc create together packinglist
- *  @access private
- **/
-const createTogetherList = async (req: Request, res: Response) => {
-  let client;
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res
-      .status(statusCode.BAD_REQUEST)
-      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
-  }
-
-  const userId: number = req.body.user.id;
-  const togetherListCreateDto: TogetherListCreateDto = req.body;
-
-  try {
-    client = await db.connect(req);
-
-    const data = await TogetherListService.createTogetherList(
-      client,
-      userId,
-      togetherListCreateDto,
-    );
-    if (!data)
-      return res
-        .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, message.FAIL_CREATE_USER));
-
-    if (data == 'exceed_limit')
-      res
-        .status(statusCode.BAD_REQUEST)
-        .send(util.success(statusCode.BAD_REQUEST, message.EXCEED_LENGTH));
-    else if (data == 'not_found_list')
-      res
-        .status(statusCode.NOT_FOUND)
-        .send(util.success(statusCode.NOT_FOUND, message.NO_PACKINGLIST));
-    else if (data == 'not_found_group')
-      res.status(statusCode.NOT_FOUND).send(util.success(statusCode.NOT_FOUND, message.NO_GROUP));
-    else
-      res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.OK, message.CREATE_TOGETHERPACKINGLIST_SUCCESS, data));
-  } catch (error) {
-    console.log(error);
-    res
-      .status(statusCode.INTERNAL_SERVER_ERROR)
-      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
-  } finally {
-    client.release();
-  }
-};
-
-/**
  *  @route GET /list/together/:listId
  *  @desc read together packinglist
  *  @access private
