b0af34c968fe0196c6332c38577c43d9f60705fc
kkl4846 <kkl4846@cau.ac.kr>
2022-08-22 02:29:11 +0900

    [FEAT]/#31 폴더 생성 router

--- src/routes/FolderRouter.ts
+++ src/routes/FolderRouter.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { FolderController } from '../controllers';
 
 const router = Router();
 
 router.get('/recentCreatedList', FolderController.getRecentCreatedList);
-
+router.post(
+  '/',
+  [body('name').notEmpty(), body('isAloned').notEmpty()],
+  FolderController.createFolder,
+);
 export default router;
