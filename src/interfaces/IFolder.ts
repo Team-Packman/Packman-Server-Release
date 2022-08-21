d3526135363162193dbb909448e88072d4e77e78
kkl4846 <kkl4846@cau.ac.kr>
2022-08-22 02:28:12 +0900

    [ADD]/#31 폴더 생성 인터페이스 추가

---
+++ src/interfaces/IFolder.ts
@@ -0,0 +1,25 @@
+export interface FolderCreateDto {
+    name: string;
+    isAloned: boolean;
+  }
+
+export interface FolderInfoDto {
+    id: string;
+    name: string;
+    isAloned: boolean;
+    listNum: number;
+}
+  
+export interface AllFolderResponseDto {
+    aloneFolder: {
+      id: string;
+      name: string;
+      listNum: number;
+    }[];
+    togetherFolder: {
+      id: string;
+      name: string;
+      listNum: number;
+    }[];
+  }
+  
