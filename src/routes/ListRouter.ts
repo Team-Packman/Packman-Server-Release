6496f7ee3b3eda218eff4aa38624418da3cafff3
laalaa31 <jseoh4298@gmail.com>
2022-08-23 01:45:39 +0900

    [FEAT]/#65 패킹리스트 출발 날짜 수정 router 연결

--- src/routes/ListRouter.ts
+++ src/routes/ListRouter.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 import { ListController } from '../controllers';
 import auth from '../middlewares/auth';
+import { body } from 'express-validator';
+
 const router = Router();
 
 router.get('/invite/:inviteCode', auth, ListController.inviteList);
+router.patch(
+  '/departureDate',
+  [body('id').notEmpty(), body('departureDate').notEmpty(), body('isAloned').notEmpty()],
+  auth,
+  ListController.updateListDate,
+);
+
 export default router;
