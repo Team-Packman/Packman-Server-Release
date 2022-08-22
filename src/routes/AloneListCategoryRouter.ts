62c8681b5834bf3ceadfd7b59fc028b568a18410
kkl4846 <kkl4846@cau.ac.kr>
2022-08-22 16:37:02 +0900

    [FEAT]/#50 혼자 패킹 카테고리 수정 router

---
+++ src/routes/AloneListCategoryRouter.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { body } from 'express-validator';
+import { AloneListCategoryController } from '../controllers';
+import auth from '../middlewares/auth';
+
+const router: Router = Router();
+
+
+router.patch(
+    '/',
+    [body('id').notEmpty(), body('name').notEmpty(), body('listId').notEmpty()],
+ 
+    AloneListCategoryController.updateCategory,
+  );
+
+
+export default router;
