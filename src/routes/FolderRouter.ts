ebd34ae71f5adcd7f6d4a1b84984c67c1ce14379
dingding-21 <phj8221@naver.com>
2022-08-22 13:45:31 +0900

    [FEAT]/#39 폴더 속 함께 패킹리스트 조회 라우터 분리

--- src/routes/FolderRouter.ts
+++ src/routes/FolderRouter.ts
@@ -4,5 +4,6 @@ import { FolderController } from '../controllers';
 const router = Router();
 
 router.get('/recentCreatedList', FolderController.getRecentCreatedList);
+router.get('/list/together/:folderId', FolderController.getTogetherListInFolder);
 
 export default router;
