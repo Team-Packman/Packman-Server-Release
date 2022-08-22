dc060bfe3b780283bfd63b295e6a48d2f922858f
kkl4846 <kkl4846@cau.ac.kr>
2022-08-22 13:46:45 +0900

    [FEAT]/#47 폴더 조회 controller

--- src/controllers/FolderController.ts
+++ src/controllers/FolderController.ts
@@ -34,6 +34,30 @@ const getRecentCreatedList = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @route GET /folder
+ *  @desc read user folder
+ *  @access private
+ **/
+
+ const getFolders = async (req: Request, res: Response) => {
+  let client;
+  const userId = req.body.user.id;
+  try {
+    client = await db.connect(req);
+    const data = await FolderService.getFolders(client, userId);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, message.SUCCESS_GET_FOLDERS, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  } finally {
+    if (client !== undefined) client.release();
+  }
+};
+
 export default {
   getRecentCreatedList,
+  getFolders,
 };
