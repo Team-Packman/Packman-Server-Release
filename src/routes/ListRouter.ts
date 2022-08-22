04da37c39d9c787ccd2c26b351fb8120f88e8c02
laalaa31 <jseoh4298@gmail.com>
2022-08-23 00:31:15 +0900

    [CHORE]/#63 패킹리스트 제목 수정 router 연결

--- src/routes/ListRouter.ts
+++ src/routes/ListRouter.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 import { ListController } from '../controllers';
 import auth from '../middlewares/auth';
+import { body } from 'express-validator';
+
 const router = Router();
 
 router.get('/invite/:inviteCode', auth, ListController.inviteList);
+router.patch(
+  '/title',
+  [body('id').notEmpty(), body('title').notEmpty(), body('isAloned').notEmpty()],
+  auth,
+  ListController.updateListTitle,
+);
+
 export default router;
