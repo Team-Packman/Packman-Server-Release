e6ee9d86ce9261f57469667d88d0d59d906d72c6
laalaa31 <jseoh4298@gmail.com>
2022-08-23 01:38:59 +0900

    [FEAT]/#65 패킹리스트 출발 날짜 수정 service 구현

--- src/services/ListService.ts
+++ src/services/ListService.ts
@@ -1,4 +1,4 @@
-import { ListInviteResponseDto } from '../interfaces/IList';
+import { ListDateUpdateDto, ListInviteResponseDto } from '../interfaces/IList';
 
 const getPackingByInviteCode = async (
   client: any,
@@ -26,6 +26,75 @@ const getPackingByInviteCode = async (
   }
 };
 
+const updateListDate = async (
+  client: any,
+  listDateUpdateDto: ListDateUpdateDto,
+): Promise<ListDateUpdateDto | string> => {
+  try {
+    let updatedDate;
+
+    if (listDateUpdateDto.isAloned === true) {
+      const { rows: existList } = await client.query(
+        `
+        SELECT *
+        FROM "alone_packing_list" as l
+        JOIN "packing_list" p ON l.id=p.id
+        WHERE l.id=$1 AND l.is_aloned=true AND p.is_deleted=false
+        `,
+        [listDateUpdateDto.id],
+      );
+      if (existList.length === 0) return 'no_list';
+
+      const { rows: updatedData } = await client.query(
+        `
+        UPDATE "packing_list"
+        SET departure_date=$1
+        WHERE id=$2
+        RETURNING TO_CHAR(departure_date,'YYYY.MM.DD') AS "departureDate"
+        `,
+        [listDateUpdateDto.departureDate, listDateUpdateDto.id],
+      );
+      updatedDate = updatedData[0].departureDate;
+    } else {
+      const { rows: existList } = await client.query(
+        `
+        SELECT together_packing_list_id, my_packing_list_id
+        FROM "together_alone_packing_list" as l
+        JOIN "packing_list" p ON l.together_packing_list_id=p.id OR l.my_packing_list_id=p.id
+        WHERE l.id=$1 AND p.is_deleted=false
+        `,
+        [listDateUpdateDto.id],
+      );
+      if (existList.length < 2) return 'no_list';
+
+      const togetherListId = existList[0].together_packing_list_id;
+      const aloneListId = existList[0].my_packing_list_id;
+
+      const { rows: updatedData } = await client.query(
+        `
+        UPDATE "packing_list"
+        SET departure_date=$1
+        WHERE id=$2 OR id=$3
+        RETURNING TO_CHAR(departure_date,'YYYY.MM.DD') AS "departureDate"
+        `,
+        [listDateUpdateDto.departureDate, togetherListId, aloneListId],
+      );
+      updatedDate = updatedData[0].departureDate;
+    }
+
+    const data = {
+      id: listDateUpdateDto.id,
+      departureDate: updatedDate,
+    };
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 export default {
   getPackingByInviteCode,
+  updateListDate,
 };
