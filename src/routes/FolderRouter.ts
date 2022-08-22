167d808a287d60934b813320b3f572c792d71314
dingding-21 <phj8221@naver.com>
2022-08-22 17:50:17 +0900

    [FEAT]/#54 폴더 속 혼자 패킹리스트 조회 라우터 분리

--- src/routes/FolderRouter.ts
+++ src/routes/FolderRouter.ts
@@ -5,5 +5,6 @@ import auth from '../middlewares/auth';
 const router = Router();
 
 router.get('/recentCreatedList', auth, FolderController.getRecentCreatedList);
+router.get('/list/alone/:folderId', auth, FolderController.getAloneListInFolder);
 
 export default router;
