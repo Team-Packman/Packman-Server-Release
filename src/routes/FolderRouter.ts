42a53f4267f27ab96dccd26d5afff4dc9c139bb7
dingding-21 <phj8221@naver.com>
2022-08-22 17:10:07 +0900

    [FEAT]/#39 접근이 private한 api에 auth추가

--- src/routes/FolderRouter.ts
+++ src/routes/FolderRouter.ts
@@ -4,7 +4,7 @@ import auth from '../middlewares/auth';
 
 const router = Router();
 
-router.get('/recentCreatedList', FolderController.getRecentCreatedList);
-router.get('/list/together/:folderId', FolderController.getTogetherListInFolder);
+router.get('/recentCreatedList', auth, FolderController.getRecentCreatedList);
+router.get('/list/together/:folderId', auth, FolderController.getTogetherListInFolder);
 
 export default router;
