d687ac82f52d86f6457d33a0e43157445df6e0a2
kkl4846 <kkl4846@cau.ac.kr>
2022-08-22 16:36:26 +0900

    [FEAT]/#50 혼자 패킹 카테고리 수정 controller

---
+++ src/controllers/AloneListCategoryController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from 'express';
+import statusCode from '../modules/statusCode';
+import message from '../modules/responseMessage';
+import util from '../modules/util';
+import { validationResult } from 'express-validator';
+import { CategoryUpdateDto } from '../interfaces/ICategory';
+import db from '../loaders/db';
+import { AloneListCategoryService } from '../services';
+
+/**
+ *  @route PATCH /category
+ *  @desc update category
+ *  @access private
+ **/
+ const updateCategory = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+    }
+    let client;
+    const categoryUpdateDto: CategoryUpdateDto = req.body;
+  
+    try {
+      client = await db.connect(req);
+      const data = await AloneListCategoryService.updateCategory(client, categoryUpdateDto);
+  
+      if (data === 'no_list') {
+        res
+          .status(statusCode.NOT_FOUND)
+          .send(util.fail(statusCode.NOT_FOUND, message.NO_PACKINGLIST));
+      } else if (data === 'no_category') {
+        res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NO_CATEGORY));
+      } else if (data === 'no_list_category') {
+        res
+          .status(statusCode.BAD_REQUEST)
+          .send(util.fail(statusCode.BAD_REQUEST, message.NO_LIST_CATEGORY));
+      } else if (data === 'duplicated_category') {
+        res
+          .status(statusCode.BAD_REQUEST)
+          .send(util.fail(statusCode.BAD_REQUEST, message.DUPLICATED_CATEGORY));
+      } else if (data === 'exceed_len') {
+        res
+          .status(statusCode.BAD_REQUEST)
+          .send(util.fail(statusCode.BAD_REQUEST, message.EXCEED_LENGTH));
+      } else {
+        res
+          .status(statusCode.OK)
+          .send(util.success(statusCode.OK, message.UPDATE_ALONE_CATEGORY_SUCCESS, data));
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    } finally {
+      if (client !== undefined) client.release();
+    }
+  };
+
+  export default {
+      updateCategory,
+  }
