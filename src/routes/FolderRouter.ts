c94c065d5272e5ad1e7e04445ef96301611e2434
kkl4846 <kkl4846@cau.ac.kr>
2022-08-22 13:47:41 +0900

    [FEAT]/#47 폴더 조회 router

--- src/routes/FolderRouter.ts
+++ src/routes/FolderRouter.ts
@@ -5,5 +5,6 @@ import auth from '../middlewares/auth';
 const router = Router();
 
 router.get('/recentCreatedList', auth, FolderController.getRecentCreatedList);
+router.get('/', auth, FolderController.getFolders);
 
 export default router;
