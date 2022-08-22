92b7ee1ebab0b46e319e717dbbb490c865b8cd61
laalaa31 <jseoh4298@gmail.com>
2022-08-22 19:40:49 +0900

    [FEAT]/#61 혼자 패킹리스트 조회 service 구현

---
+++ src/services/AloneListService.ts
@@ -0,0 +1,49 @@
+import { aloneCategoryResponse } from '../modules/aloneCategoryResponse';
+import { AloneListResponseDto } from '../interfaces/IAloneList';
+
+const readAloneList = async (
+  client: any,
+  listId: string,
+): Promise<AloneListResponseDto | string> => {
+  try {
+    const { rows: existList } = await client.query(
+      `
+      SELECT *
+      FROM "alone_packing_list" as l
+      JOIN "packing_list" p ON l.id=p.id
+      WHERE l.id=$1 AND l.is_aloned=true AND p.is_deleted=false
+      `,
+      [listId],
+    );
+    if (existList.length === 0) return 'no_list';
+
+    const { rows: etcDataArray } = await client.query(
+      `
+      SELECT p.title AS "title", TO_CHAR(p.departure_date,'YYYY.MM.DD') AS "departureDate", p.is_templated AS "isSaved"
+      FROM "packing_list" p
+      WHERE p.id=$1
+      `,
+      [listId],
+    );
+    const etcData = etcDataArray[0];
+
+    const category = await aloneCategoryResponse(client, listId);
+
+    const data: AloneListResponseDto = {
+      id: listId.toString(),
+      title: etcData.title,
+      departureDate: etcData.departureDate,
+      category: category,
+      isSaved: etcData.isSaved,
+    };
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default {
+  readAloneList,
+};
