e5c6df87acc2283643f5240aa4f1381ddeb24755
dingding-21 <phj8221@naver.com>
2022-08-23 15:36:26 +0900

    [FEAT]/#59 혼자 패킹리스트 짐 삭제 service 구현

---
+++ src/services/AloneListPackService.ts
@@ -0,0 +1,88 @@
+import { PackDeleteDto } from '../interfaces/IPack';
+import { AloneListCategoryResponseDto } from '../interfaces/IAloneList';
+import { aloneCategoryResponse } from '../modules/aloneCategoryResponse';
+
+const deletePack = async (
+  client: any,
+  packDeleteDto: PackDeleteDto,
+): Promise<AloneListCategoryResponseDto | string> => {
+  try {
+    const { rows: existList } = await client.query(
+      `
+      SELECT *
+      FROM "packing_list" as pl
+      JOIN alone_packing_list apl on pl.id = apl.id
+      WHERE apl.id = $1 and pl.is_deleted = false
+      `,
+      [packDeleteDto.listId],
+    );
+
+    if (existList.length === 0) return 'no_list';
+
+    const { rows: existCategory } = await client.query(
+      `
+      SELECT *
+      FROM "category" as c
+      WHERE c.id = $1
+      `,
+      [packDeleteDto.categoryId],
+    );
+
+    if (existCategory.length === 0) return 'no_category';
+
+    const { rows: existPack } = await client.query(
+      `
+      SELECT *
+      FROM "pack" as p
+      WHERE p.id = $1
+      `,
+      [packDeleteDto.packId],
+    );
+
+    if (existPack.length === 0) return 'no_pack';
+
+    const { rows: existListCategory } = await client.query(
+      `
+      SELECT *
+      FROM "category" as c
+      WHERE c.id = $1 AND c.list_id = $2
+      `,
+      [packDeleteDto.categoryId, packDeleteDto.listId],
+    );
+
+    if (existListCategory.length === 0) return 'no_list_category';
+    const { rows: existCategoryPack } = await client.query(
+      `
+      SELECT *
+      FROM "pack" as p
+      WHERE p.id = $1 AND p.category_id = $2
+      `,
+      [packDeleteDto.packId, packDeleteDto.categoryId],
+    );
+
+    if (existCategoryPack.length === 0) return 'no_category_pack';
+
+    const { rows } = await client.query(
+      `
+      DELETE FROM "pack"
+      WHERE id = $1
+      `,
+      [packDeleteDto.packId],
+    );
+
+    const category = await aloneCategoryResponse(client, packDeleteDto.listId);
+
+    const aloneListCategoryResponseDto: AloneListCategoryResponseDto = {
+      id: packDeleteDto.listId,
+      category: category,
+    };
+    return aloneListCategoryResponseDto;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default {
+  deletePack,
+};
