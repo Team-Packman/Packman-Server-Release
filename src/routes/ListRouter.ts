3c9ffba612fbd8af68f419bddff032895dd9b1fb
laalaa31 <jseoh4298@gmail.com>
2022-08-23 14:56:37 +0900

    [FEAT]/#67 나의 템플릿 추가 및 업데이트 router 연결

--- src/routes/ListRouter.ts
+++ src/routes/ListRouter.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 import { ListController } from '../controllers';
 import auth from '../middlewares/auth';
+import { body } from 'express-validator';
+
 const router = Router();
 
 router.get('/invite/:inviteCode', auth, ListController.inviteList);
+router.patch(
+  '/myTemplate',
+  [body('id').notEmpty(), body('isSaved').notEmpty(), body('isAloned').notEmpty()],
+  auth,
+  ListController.updateMyTemplate,
+);
+
 export default router;
