32e0b0c560647434db48eba88feed14f51018ff3
laalaa31 <jseoh4298@gmail.com>
2022-08-25 02:04:26 +0900

    [FEAT]/#79 response template list 모듈화

---
+++ src/modules/templateListRespones.ts
@@ -0,0 +1,40 @@
+import { TemplateListResponseDto } from '../interfaces/ITemplate';
+
+async function templateListResponse(
+  client: any,
+  userId: string,
+  isAloned: boolean,
+): Promise<TemplateListResponseDto> {
+  try {
+    const { rows: basicTemplateList } = await client.query(
+      `
+    	SELECT t.id::text, t.title
+      FROM template t
+			WHERE user_id IS NULL AND is_deleted=false
+			ORDER BY t.id
+			`,
+    );
+
+    const { rows: myTemplateList } = await client.query(
+      `
+			SELECT t.id::text, t.title
+			FROM template t
+			WHERE user_id=$1 AND is_aloned=${isAloned} AND is_deleted=false
+			ORDER BY t.id
+			`,
+      [userId],
+    );
+
+    const templateList: TemplateListResponseDto = {
+      basicTemplate: basicTemplateList,
+      myTemplate: myTemplateList,
+    };
+
+    return templateList;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export { templateListResponse };
--- src/services/TemplateService.ts
+++ src/services/TemplateService.ts
@@ -1,34 +1,18 @@
 import { TemplateListResponseDto } from '../interfaces/ITemplate';
+import { templateListResponse } from '../modules/templateListRespones';
 
 const getAloneTemplateList = async (
   client: any,
   userId: string,
 ): Promise<TemplateListResponseDto | string> => {
   try {
-    const { rows: basicTemplateList } = await client.query(
-      `
-    	SELECT t.id::text, t.title
-      FROM template t
-			WHERE user_id IS NULL AND is_deleted=false
-			ORDER BY t.id
-			`,
+    const aloneTemplateList: TemplateListResponseDto = await templateListResponse(
+      client,
+      userId,
+      true,
     );
 
-    const { rows: myTemplateList } = await client.query(
-      `
-			SELECT t.id::text, t.title
-			FROM template t
-			WHERE user_id=$1 AND is_aloned=true AND is_deleted=false
-			`,
-      [userId],
-    );
-
-    const data: TemplateListResponseDto = {
-      basicTemplate: basicTemplateList,
-      myTemplate: myTemplateList,
-    };
-
-    return data;
+    return aloneTemplateList;
   } catch (error) {
     console.log(error);
     throw error;
