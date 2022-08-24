7e3b22624e875bd2c4db99c1a4618ac8ee41cc1c
dingding-21 <phj8221@naver.com>
2022-08-24 15:17:45 +0900

    [ADD]#35 router에 auth 추가

--- src/routes/FolderRouter.ts
+++ src/routes/FolderRouter.ts
@@ -5,6 +5,6 @@ import auth from '../middlewares/auth';
 const router = Router();
 
 router.get('/recentCreatedList', auth, FolderController.getRecentCreatedList);
-router.get('/alone', FolderController.getAloneFolders);
+router.get('/alone', auth, FolderController.getAloneFolders);
 
 export default router;
