0aba5d9eea60550c8f1d79f267b75a8e63ad2f1b
kkl4846 <kkl4846@cau.ac.kr>
2022-08-24 11:58:33 +0900

    [FEAT]/#48 혼자 패킹 id 존재 확인하는 예외처리 추가

--- src/services/AloneListCategoryService.ts
+++ src/services/AloneListCategoryService.ts
@@ -14,8 +14,9 @@ const createCategory = async (
       `
         SELECT *
         FROM "packing_list" as pl
-        WHERE pl.id = $1 AND pl.is_deleted = false
-     `,
+        JOIN alone_packing_list apl on pl.id = apl.id
+        WHERE apl.id = $1 and pl.is_deleted = false
+      `,
       [categoryCreateDto.listId],
     );
     if (existList.length === 0) {
