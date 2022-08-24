575f67c6890a20d4a44e61a1f482dd48e790ade8
laalaa31 <jseoh4298@gmail.com>
2022-08-25 02:27:13 +0900

    [FEAT]/#80 함께 패킹 카테고리 리스트 조회 service 구현

---
+++ src/services/TemplateService.ts
@@ -0,0 +1,24 @@
+import { TemplateListResponseDto } from '../interfaces/ITemplate';
+import { templateListResponse } from '../modules/TemplateListResponse';
+
+const getTogetherTemplateList = async (
+  client: any,
+  userId: string,
+): Promise<TemplateListResponseDto | string> => {
+  try {
+    const togetherTemplateList: TemplateListResponseDto = await templateListResponse(
+      client,
+      userId,
+      false,
+    );
+
+    return togetherTemplateList;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default {
+  getTogetherTemplateList,
+};
