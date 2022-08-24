e5008ed53faf53d3e5db0dbf6e49e76bc4912a0c
dingding-21 <phj8221@naver.com>
2022-08-24 15:03:12 +0900

    [CHORE]/#34 client undefined 처리

--- src/controllers/FolderController.ts
+++ src/controllers/FolderController.ts
@@ -59,7 +59,7 @@ const getTogetherFolders = async (req: Request, res: Response) => {
       .status(statusCode.INTERNAL_SERVER_ERROR)
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
   } finally {
-    client.release();
+    if (client !== undefined) client.release();
   }
 };
 
