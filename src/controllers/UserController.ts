4af6cfb3e715ad855533dd39335ef4eeac8de43c
dingding-21 <phj8221@naver.com>
2022-08-26 01:29:16 +0900

    [FEAT]/#91 유저 조회 controller 구현

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -46,6 +46,33 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @route GET /user
+ *  @desc get user
+ *  @access public
+ **/
+const getUser = async (req: Request, res: Response) => {
+  let client;
+
+  const userId = req.body.user.id;
+
+  try {
+    client = await db.connect(req);
+
+    const data = await UserService.getUser(client, userId);
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, message.SUCCESS_GET_USER, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  } finally {
+    if (client !== undefined) client.release();
+  }
+};
+
 export default {
   createUser,
+  getUser,
 };
