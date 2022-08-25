ef6f88c51a855c75bb6cfde7f980ebba3ed83738
kkl4846 <kkl4846@cau.ac.kr>
2022-08-26 02:26:05 +0900

    [FEAT]/#95 유저 정보 수정 controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -46,6 +46,51 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @route PATCH /user/profile
+ *  @desc update user
+ *  @access private
+ **/
+ const updateUser = async (req: Request, res: Response) => {
+  let client;
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+  }
+  const userId = '1';
+  const userUpdateDto: UserUpdateDto = req.body;
+
+  try {
+    client = await db.connect(req);
+
+    const data = await UserService.updateUser(client, userUpdateDto, userId);
+
+    if (data === 'exceed_len') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.success(statusCode.BAD_REQUEST, message.EXCEED_LENGTH));
+    } else if (data === 'no_user') {
+      res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.success(statusCode.BAD_REQUEST, message.NO_USER));
+    } else {
+      res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.SUCCESS_UPDATE_USER, data));
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  } finally {
+    if (client !== undefined) client.release();
+  }
+};
+
 export default {
   createUser,
+  updateUser,
 };
