95312b7e3651a21220035a232c465c10089b34de
kkl4846 <kkl4846@cau.ac.kr>
2022-08-25 15:43:45 +0900

    [FEAT]/#84 폴더 수정 controller

--- src/controllers/FolderController.ts
+++ src/controllers/FolderController.ts
@@ -74,6 +74,50 @@ const getRecentCreatedList = async (req: Request, res: Response) => {
   }
 };
 
+
+/**
+ *  @route PATCH /folder
+ *  @desc update folder
+ *  @access private
+ **/
+ const updateFolder = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+  }
+  let client;
+  const userId = '1';
+  const folderUpdateDto = req.body;
+  try {
+    client = await db.connect(req);
+    const data = await FolderService.updateFolder(client, userId, folderUpdateDto);
+    if (data === 'exceed_len') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.EXCEED_LENGTH));
+    } else if (data === 'no_folder') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_FOLDER));
+    } else {
+      res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.SUCCESS_UPDATE_FOLDER, data));
+    }
+  } catch (error) {
+    
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  } finally {
+    if (client !== undefined) client.release();
+  }
+};
+
+
 /**
  *  @route GET /folder
  *  @desc read user folder
@@ -228,6 +272,7 @@ export default {
   getRecentCreatedList,
   createFolder,
   getFolders,
+  updateFolder,
   getTogetherFolders,
   getAloneFolders,
   getTogetherListInFolder,
