546fb9ab17db1f9e79d82cd98daa12f184a674ee
dingding-21 <phj8221@naver.com>
2022-08-31 03:38:55 +0900

    [FEAT]/#107 함께 패킹 멤버 삭제 controller 구현

--- src/controllers/MemberController.ts
+++ src/controllers/MemberController.ts
@@ -27,9 +27,55 @@ const getMember = async (req: Request, res: Response) => {
         .status(statusCode.BAD_REQUEST)
         .send(util.fail(statusCode.BAD_REQUEST, message.NO_MEMBER_USER));
     } else {
+      res.status(statusCode.OK).send(util.success(statusCode.OK, message.SUCCESS_GET_MEMBER, data));
+    }
+  } catch (error) {
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  } finally {
+    if (client !== undefined) client.release();
+  }
+};
+
+/**
+ *  @route DELETE /member/:groupId/:memberId
+ *  @desc delete member in group
+ *  @access private
+ **/
+const deleteMember = async (req: Request, res: Response) => {
+  let client;
+
+  const { groupId, memberId } = req.params;
+  const userId = req.body.user.id;
+
+  try {
+    client = await db.connect(req);
+
+    const data = await MemberService.deleteMember(client, userId, groupId, memberId);
+
+    if (data === 'no_user') {
+      res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NO_USER));
+    } else if (data === 'no_group') {
+      res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NO_GROUP));
+    } else if (data === 'empty_member') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.EMPTY_MEMBER));
+    } else if (data === 'no_maker') {
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NO_MAKER));
+    } else if (data === 'no_delete_maker') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_DELETE_MAKER));
+    } else if (data === 'no_member_user') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_MEMBER_USER));
+    } else {
       res
         .status(statusCode.OK)
-        .send(util.success(statusCode.OK, message.SUCCESS_GET_MEMBERS, data));
+        .send(util.success(statusCode.OK, message.SUCCESS_DELETE_MEMBER, data));
     }
   } catch (error) {
     res
@@ -42,4 +88,5 @@ const getMember = async (req: Request, res: Response) => {
 
 export default {
   getMember,
+  deleteMember,
 };
