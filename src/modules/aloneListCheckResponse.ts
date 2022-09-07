33ca927ceb4fb0a684b97603d1de6515558901f6
laalaa31 <jseoh4298@gmail.com>
2022-09-08 01:06:38 +0900

    [CHORE]/#135 성능 같은 쿼리문 가독성 향상

--- src/modules/aloneListCheckResponse.ts
+++ src/modules/aloneListCheckResponse.ts
@@ -8,13 +8,13 @@ async function aloneListCheckResponse(
   try {
     const { rows: existList } = await client.query(
       `
-        	SELECT pl.title,TO_CHAR(pl.departure_date,'YYYY-MM-DD') AS "departureDate",
-        	       pl.is_saved AS "isSaved", apl.invite_code AS "inviteCode"
-        	FROM "folder" f
-        	JOIN "folder_packing_list" fpl ON f.id=fpl.folder_id AND f.user_id=$1 AND f.is_aloned=true
-        	JOIN "packing_list" pl ON fpl.list_id=pl.id
-        	JOIN "alone_packing_list" apl ON pl.id=apl.id
-        	WHERE pl.is_deleted=false AND apl.id=$2 AND apl.is_aloned=true
+        SELECT pl.title,TO_CHAR(pl.departure_date,'YYYY-MM-DD') AS "departureDate",
+               pl.is_saved AS "isSaved", apl.invite_code AS "inviteCode"
+        FROM "folder" f
+        JOIN "folder_packing_list" fpl ON f.id=fpl.folder_id
+        JOIN "packing_list" pl ON fpl.list_id=pl.id
+        JOIN "alone_packing_list" apl ON pl.id=apl.id
+        WHERE f.user_id=$1 AND f.is_aloned=true AND pl.is_deleted=false AND apl.id=$2 AND apl.is_aloned=true
       `,
       [userId, listId],
     );
--- src/modules/togetherListCheckResponse.ts
+++ src/modules/togetherListCheckResponse.ts
@@ -11,10 +11,10 @@ async function togetherListCheckResponse(
         SELECT pl.title,TO_CHAR(pl.departure_date,'YYYY-MM-DD') AS "departureDate", pl.is_saved AS "isSaved",
                tapl.my_packing_list_id::text AS "myListId", tapl.together_packing_list_id::text AS "togetherListId"
         FROM "folder" f
-        JOIN "folder_packing_list" fpl ON f.id=fpl.folder_id AND f.user_id=$1 AND f.is_aloned=false
+        JOIN "folder_packing_list" fpl ON f.id=fpl.folder_id
         JOIN "together_alone_packing_list" tapl ON fpl.list_id=tapl.my_packing_list_id
         JOIN "packing_list" pl ON tapl.together_packing_list_id=pl.id OR tapl.my_packing_list_id=pl.id
-        WHERE tapl.id=$2 AND pl.is_deleted=false
+        WHERE f.user_id=$1 AND f.is_aloned=false AND tapl.id=$2 AND pl.is_deleted=false
         ORDER BY pl.id
       `,
       [userId, listId],
