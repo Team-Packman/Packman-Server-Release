a578fdbc93ca60e61d2caa47bb07029df72d3a97
dingding-21 <phj8221@naver.com>
2022-09-19 19:33:39 +0900

    [ADD]/#155 controller의 catch문의 console.log 추가

--- src/controllers/MemberController.ts
+++ src/controllers/MemberController.ts
@@ -37,6 +37,7 @@ const getMember = async (req: Request, res: Response) => {
       res.status(statusCode.OK).send(util.success(statusCode.OK, message.SUCCESS_GET_MEMBER, data));
     }
   } catch (error) {
+    console.log(error);
     res
       .status(statusCode.INTERNAL_SERVER_ERROR)
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
@@ -85,6 +86,7 @@ const deleteMember = async (req: Request, res: Response) => {
         .send(util.success(statusCode.OK, message.SUCCESS_DELETE_MEMBER, data));
     }
   } catch (error) {
+    console.log(error);
     res
       .status(statusCode.INTERNAL_SERVER_ERROR)
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
--- src/controllers/TemplateController.ts
+++ src/controllers/TemplateController.ts
@@ -23,6 +23,7 @@ const getAloneTemplateList = async (req: Request, res: Response) => {
       .status(statusCode.OK)
       .send(util.success(statusCode.OK, message.GET_ALONE_TEMPLATE_SUCCESS, data));
   } catch (error) {
+    console.log(error);
     res
       .status(statusCode.INTERNAL_SERVER_ERROR)
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
@@ -49,6 +50,7 @@ const getTogetherTemplateList = async (req: Request, res: Response) => {
       .status(statusCode.OK)
       .send(util.success(statusCode.OK, message.GET_TOGETHER_TEMPLATE_SUCCESS, data));
   } catch (error) {
+    console.log(error);
     res
       .status(statusCode.INTERNAL_SERVER_ERROR)
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
