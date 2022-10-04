569d264ca46cff42b5c30729ffc449152980d004
dingding-21 <phj8221@naver.com>
2022-10-04 20:35:35 +0900

    [FEAT]/#170 엿보기 조회 로그

--- src/services/HelpService.ts
+++ src/services/HelpService.ts
@@ -1,5 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const getHelp = async (client: any) => {};
+import logger from '../config/logger';
+
+const getHelp = async (client: any, userId: number) => {
+  logger.logger.info(`GET, /help, 엿보기 조회, 200, userId: ${userId}`);
+};
 
 export default {
   getHelp,
